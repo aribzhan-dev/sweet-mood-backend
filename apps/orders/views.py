4ad91e638aa76ca938e0f4f5a026431f3cea5318
Aribzhan <aribzankamilzanov@gmail.com>
2026-03-18 13:28:50 +0500

    new fix

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,6 +1,9 @@
+import json
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
 from django.db import transaction
+from django.shortcuts import get_object_or_404
 
 from .models import Order, OrderItem
 from .serializers import OrderCreateSerializer
@@ -13,37 +16,55 @@ class OrderViewSet(viewsets.GenericViewSet):
     serializer_class = OrderCreateSerializer
     http_method_names = ["post"]
 
+    parser_classes = (MultiPartParser, FormParser)
+
     @transaction.atomic
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        items_data = serializer.validated_data.pop("items")
+        items_data = request.data.get("items")
+
+        try:
+            items_data = json.loads(items_data)
+        except Exception:
+            return Response(
+                {"error": "Items must be valid JSON"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
         order = Order.objects.create(
-            total_price=0,
-            **serializer.validated_data
+            name=serializer.validated_data["name"],
+            surname=serializer.validated_data["surname"],
+            phone_number=serializer.validated_data["phone_number"],
+            order_type=serializer.validated_data["order_type"],
+            location=serializer.validated_data["location"],
+            payment_check_file=serializer.validated_data["payment_check_file"],
+            total_price=0
         )
+
         total_price = 0
-        created_items = []
 
         for item in items_data:
-            product_id = item["product_id"]
-            quantity = item["quantity"]
-            product = Product.objects.get(id=product_id)
+
+            product = get_object_or_404(Product, id=item["product_id"])
+
+            quantity = int(item["quantity"])
+
             price = product.actual_price()
-            order_item = OrderItem.objects.create(
+
+            OrderItem.objects.create(
                 order=order,
                 product=product,
                 quantity=quantity,
                 price=price
             )
 
-            created_items.append(order_item)
             total_price += price * quantity
 
         order.total_price = total_price
         order.save()
 
         send_order_to_telegram(order)
+
         return Response(
             {
                 "message": "Order created successfully",
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path("api/products/", include("apps.products.urls")),
     path("api/orders/", include("apps.orders.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
